7f1a2a4f6a3cb89c3c8fd369b03e76d5ac5926cc
Drew Noel <drewkunpwns@gmail.com>
2013-03-17 10:41:36 -0400

    Fix some style issues, a few typos

--- example/readTest.java
+++ example/readTest.java
@@ -1,18 +1,19 @@
-import java.util.*;
+import java.util.List;
 
-public class readTest {
-	
+public class readTest
+{
 
-	public static void main(String[] args)
-	{	
-		fileReader reader = new fileReader("readme", "txt"); // looks for and reads index.htm
-		List<String> lines = reader.getList(); // gets you lines of the read file
-		List<String> tokens = reader.getTokens(","); // gets you list of word delimited by ","
-		
-		int fileOne = reader.initiateStream("output");
-		int fileTwo = reader.initiateStream("output2");
-		int fileThree = reader.initiateStream("output3");
-		
+	@SuppressWarnings("unused")
+	public static void main(final String[] args)
+	{
+		final fileReader reader = new fileReader("readme", "txt"); // looks for and reads readme.txt
+		final List<String> lines = reader.getList(); // gets you lines of the read file
+		final List<String> tokens = reader.getTokens(","); // gets you list of word delimited by ","
+
+		final int fileOne = reader.initiateStream("output");
+		final int fileTwo = reader.initiateStream("output2");
+		final int fileThree = reader.initiateStream("output3");
+
 		reader.print(fileOne, "Okay"); // prints "Okay" to file output.txt
 		reader.close(); // closes all PrintStreams
 	}
--- src/fileReader.java
+++ src/fileReader.java
@@ -1,127 +1,139 @@
-import java.io.*;
-import java.util.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
 
-/** This class represents a reader that redirects STD I/O to current directory and reads/writes files in current directory. */
-public class fileReader {
-	
-	private LinkedList <String> list = new LinkedList<String>();
-	private Set <String> set = new LinkedHashSet<String>();
-	private LinkedList<String> tokens = new LinkedList<String>();
-	private String filename;
-	private List <PrintStream> stream = new ArrayList<PrintStream>();
+/**
+ * This class represents a reader that redirects STD I/O to current directory
+ * and reads/writes files in current directory.
+ */
+public class fileReader
+{
+
+	private final LinkedList<String> list = new LinkedList<String>();
+	private final Set<String> set = new LinkedHashSet<String>();
+	private final LinkedList<String> tokens = new LinkedList<String>();
+	private final String filename;
+	private final List<PrintStream> stream = new ArrayList<PrintStream>();
 
 	/**
-	Constructs a reader that looks in current directory for the given filename. It also populates the list and set with file lines.
-	Note: It skips empty lines. trims and lowerCase() all unempty lines.
-
-	@param filename The name of file in current directory to read
-	@pre. file is in current directory
-	@exception IOException throws Exception if cannot read filename
-
-	*/
-	public fileReader(String s, String extension)
+	 * Constructs a reader that looks in current directory for the given
+	 * filename. It also populates the list and set with file lines. Note: It
+	 * skips empty lines. trims and lowerCase() all unempty lines.
+	 * 
+	 * @param filename
+	 *            The name of file in current directory to read
+	 * @pre. file is in current directory
+	 * @exception IOException
+	 *                throws Exception if cannot read filename
+	 */
+	public fileReader(final String s, final String extension)
 	{
-		
-		this.filename = "" + System.getProperty("user.dir") +"\\" + s + "." + extension; 
-		File fieldFile = new File(filename);
+
+		this.filename = "" + System.getProperty("user.dir") + "\\" + s + "."
+				+ extension;
+		final File fieldFile = new File(this.filename);
 		Scanner input = null;
-		try 
+		try
 		{
 			input = new Scanner(fieldFile);
 		}
-		catch ( IOException e)
+		catch (final IOException e)
 		{
-			System.out.println("Error openinng: " + this.filename );
+			System.out.println("Error openinng: " + this.filename);
 			System.exit(1);
 		}
-		
-		while(input.hasNextLine() )
+
+		while (input.hasNextLine())
 		{
-			String line = input.nextLine().trim().toLowerCase();
-			if ( line.length() > 0 ) // skips empty lines
+			final String line = input.nextLine().trim().toLowerCase();
+			if (line.length() > 0) // skips empty lines
 			{
-			list.add(line);
-			set.add(line);
+				this.list.add(line);
+				this.set.add(line);
 			}
 		}
 	}
 
-	/** 
+	public void close()
+	{
+		for (final PrintStream p : this.stream)
+		{
+			p.close();
+		}
+	}
+
+	/**
+	 * Returns linkedlist of read lines of type: String Note: the linkedlist
+	 * allow duplicates.
+	 */
+	public LinkedList<String> getList()
+	{
+		final LinkedList<String> r = new LinkedList<String>(this.list);
+		return r;
+	}
+
+	/**
 	 * Returns a LinkedHashSet of non-duplicated lines in the read file
 	 * 
 	 * @return LinkedHashSet of String type
 	 * 
 	 */
 
-	public Set getSet()
+	public Set<String> getSet()
 	{
-		Set<String> r = new LinkedHashSet<String>(this.set);
+		final Set<String> r = new LinkedHashSet<String>(this.set);
 		return r;
 	}
 
-	/** 
-	 * Returns linkedlist of read lines of type: String
-	 * Note: the linkedlist allow duplicates.
-	 */
-	public LinkedList<String> getList()
-	{
-		 LinkedList<String> r = new LinkedList(this.list);
-		 return r;
-	}
-
 	/**
-	 * Returns a LinkedList of String tokens delimited by the given 
+	 * Returns a LinkedList of String tokens delimited by the given
 	 */
-	public LinkedList<String> getTokens(String delimiter)
+	public LinkedList<String> getTokens(final String delimiter)
 	{
-		String delims = "[" + delimiter + "]+" ;
-		
-		for(int i = 0; i < list.size(); i++)
-		{	
-			String line = list.get(i);
-			String[] a = line.split(delims);
+		final String delims = "[" + delimiter + "]+";
+
+		for (int i = 0; i < this.list.size(); i++)
+		{
+			final String line = this.list.get(i);
+			final String[] a = line.split(delims);
 			for (int j = 0; j < a.length; j++)
 			{
-				this.tokens.add(a[j]);	
+				this.tokens.add(a[j]);
 			}
-		
+
 		}
 		return this.tokens;
-		
+
 	}
 
-	  
-	public int initiateStream(String name)
+	public int initiateStream(final String name)
 	{
 		try
 		{
-		PrintStream p = new PrintStream(name + ".txt");
-		stream.add(p);
-		//p.close();
-		
+			final PrintStream p = new PrintStream(name + ".txt");
+			this.stream.add(p);
 		}
-		catch( Exception e)
+		catch (final Exception e)
 		{
 			System.out.println("File Not Found");
 		}
-		return stream.size();
+		return this.stream.size();
 	}
 
-	public void print(int streamNumber, String s)
+	public void print(final int streamNumber, final String s)
 	{
-		PrintStream p = stream.get(streamNumber-1);
+		final PrintStream p = this.stream.get(streamNumber - 1);
 		p.println(s);
-		System.out.println("Successfully printed: \"" + s + "\" to File No. : " + streamNumber);
+		System.out.println("Successfully printed: \"" + s + "\" to File No. : "
+				+ streamNumber);
 		p.close();
-		
-	}
 
-	public void close()
-	{
-		for(PrintStream p : stream)
-		{
-			p.close();
-		}
 	}
 
 }
