7bc02f0ec7d97a6c0d0bfd92c5a358be601dba16
Drew Noel <drewkunpwns@gmail.com>
2013-03-17 11:56:23 -0400

    Updated copies of fileReader, tweaked readTest

--- example/readTest.java
+++ example/readTest.java
@@ -2,20 +2,33 @@ import java.util.List;
 
 public class readTest
 {
-
-	@SuppressWarnings("unused")
 	public static void main(final String[] args)
 	{
-		final fileReader reader = new fileReader("readme", "txt"); // looks for and reads readme.txt
+		final fileReader reader = new fileReader("readme.txt"); // looks for and reads readme.txt
 		final List<String> lines = reader.getList(); // gets you lines of the read file
 		final List<String> tokens = reader.getTokens(","); // gets you list of word delimited by ","
 
-		final int fileOne = reader.initiateStream("output");
-		final int fileTwo = reader.initiateStream("output2");
-		final int fileThree = reader.initiateStream("output3");
+		reader.initiateStream("output.txt");
+		reader.initiateStream("output2.txt");
+		reader.initiateStream("output3.txt");
 
-		reader.print(fileOne, "Okay"); // prints "Okay" to file output.txt
-		reader.close(); // closes all PrintStreams
+		// Write a string to output
+		reader.print("output.txt", "Okay");
+		
+		// Copy all lines line-by-line with line breaks added where "," are found
+		// Put it in output2
+		for (String s : tokens)
+		{
+			reader.print("output2.txt", s);
+		}
+		
+		// Copy all lines line-by-line into output3
+		for (String s : lines)
+		{
+			reader.print("output3.txt", s);
+		}
+		
+		reader.close();
 	}
 
 }
